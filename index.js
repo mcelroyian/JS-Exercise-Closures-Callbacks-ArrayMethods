66f4b1350bc1e9e7db3739b5c659b09aa08a2a16
Ian McElroy <mcelroyian@gmail.com>
2020-03-24 20:34:19 -0500

    one stretch to go

--- index.js
+++ index.js
@@ -290,8 +290,9 @@ function firstNamesAllCaps(runners) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
-  /* CODE HERE */
+function getRunnersByTShirtSize(runners, size) {
+  const list = runners.filter(s => s.shirt_size === size);
+  return list;
 }
 
 /**
@@ -305,8 +306,11 @@ function getRunnersByTShirtSize(/* CODE HERE */) {
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns a number which is the sum of the donations by all runners.
 */
-function tallyUpDonations(/* CODE HERE */) {
-  /* CODE HERE */
+function tallyUpDonations(runners) {
+  const money = runners.reduce(function(acc, val){
+    return acc + val.donation;
+  }, 0)
+  return money;
 }
 
 /////////////// CLOSURES ///////////////
